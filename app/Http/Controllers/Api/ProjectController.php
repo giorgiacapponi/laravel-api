adcd4c11cba4239f5ab5b8a10bc7f8224ddf6a09
giorgiacapponi <giorgiacapponi3@gmail.com>
2023-06-14 18:38:35 +0200

    add pagination and with

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects=Project::all();
+        $projects=Project::with (['technologies','type'])->paginate(10);
         return response()->json([
             'succes'=>true,
             'results'=>$projects
